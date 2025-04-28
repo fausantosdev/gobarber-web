4c76150e1fcceb858ba08b6cd2b22c2cd563aa86
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-04-28 11:13:29 -0300

    refactor: remove React.FC from Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { Btn } from './styles'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button: React.FC<ButtonProps> = (props) => {
+const Button = (props : ButtonProps) => {
   return <Btn type='button' {...props}>{props.children}</Btn>
 }
 
